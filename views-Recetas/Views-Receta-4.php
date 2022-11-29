bb7cceaa1a53b2c80ac3b71df84f2466308ca086
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2022-11-28 19:38:24 -0600

    NuevosCambios

--- views-Recetas/Views-Receta-4.php
+++ views-Recetas/Views-Receta-4.php
@@ -7,9 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/sidebar.css">
         <link rel="stylesheet" href="../css/all.css">
         <script src="../js/all.js"></script>
@@ -20,13 +18,36 @@
         <div class="container">
                 <div class="row">
                         <div class="col">
-                              <h2>Hola ya funciono</h2>
+                                <div class="card mt-4 mb-4 tamaño-cards-recetas border-0  shadow-lg p-3 mb-5 bg-body rounded position-absolute top-50 start-50 translate-middle">
+                                        <!---->
+                                        <div class="card-body">
+                                                <br><br><br>
+                                                <!---->
+                                                <h1 class="mt-2 mb-2">Mole de amaranto con pollo</h1>
+                                                <h5>PARA 4 PERSONAS</h5>
+                                                <h5> 3 Chiles guajillo , desvenados</h5>
+                                                <h5>3 Chiles pasilla , desvenados</h5>
+                                                <h5>3 Chiles chipotle secos, desvenados</h5>
+                                                <h5>2 tazas Amaranto tostado</h5>
+                                                <h5> 1/4 taza Ajonjolí tostado</h5>
+                                                <h1>Procedimiento</h1>
+                                                <h5>ASA los chiles, el jitomate y la cebolla en un comal. Asa en una sartén el amaranto y el ajonjolí.</h5>
+                                                <h5>LICÚA hasta obtener una pasta, en caso necesario agrega caldo de pollo.</h5>
+                                                <h5>CALIENTA el aceite y sazona el mole. Hierve sin dejar de mover y vierte más caldo. Salpimienta.</h5>
+                                                <h5>SALPIMIENTA cada pechuga y rellena con los bastones de queso. Enrolla.</h5>
+                                                <!---->
+                                                <div class="d-grid gap-2 col-6 mx-auto mt-4 mb-4">
+                                                        <a href="../index.php" class="btn btn-warning" type="button">Regresar</a>
+                                                </div>
+                                                <!---->
+                                        </div>
+                                        <!---->
+                                </div>
+
                         </div>
                 </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 
