b25a0f3bc1ceea14fac0afec0328cc5e7797af5d
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2022-12-07 13:51:32 -0600

    ActualizacionDeHistoriaDeEmpresa

--- views/Views-vistasHistoria.php
+++ views/Views-vistasHistoria.php
@@ -7,26 +7,75 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/sidebar.css">
         <link rel="stylesheet" href="../css/all.css">
+
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
         <script src="../js/all.js"></script>
         <title>Views-vistasHistoria.php!</title>
 </head>
 
-<body>
+<body style="background-color:#000000;" class="ImgFondoVistaHistoria">
         <div class="container">
                 <div class="row">
                         <div class="col">
-                                <h2>Hola ya estas dentro de la seccion de historia</h2>
+                                <h2 class="text-center mt-4 mb-4 tipo-Letra-Titulo text-white" data-aos="fade-in" data-aos-duration="4000">HISTORIA DE MOLES TRADICIONALES RIOS</h2>
+                                <!---->
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <!---->
+                                <div class="row">
+                                        <div class="col-sm-6">
+                                                <div class="card colorTransparenteCards rounded border-0" data-aos="fade-up" data-aos-duration="4000">
+                                                        <div class="card-body">
+                                                                <br><br>
+                                                        <h5 class="text-white tipo-letra mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                                <div class="card colorTransparenteCards rounded border-0" data-aos="fade-up" data-aos-duration="4000">
+                                                        <div class="card-body">
+                                                        <iframe  width="560" height="315" src="https://www.youtube.com/embed/Hwk1BjuNk-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                                <!---->
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <!---->
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <!---->
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <!---->
+                                <h5 class="text-white tipo-letra mt-4" data-aos="fade-up" data-aos-duration="4000">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, cum? Tempore recusandae, dolore ipsum at doloremque magnam optio reprehenderit. Commodi fugiat aspernatur, facere veniam itaque laboriosam accusamus at architecto eius.
+                                        Omnis quibusdam reiciendis dolore excepturi quas non molestiae, amet explicabo, quod, minus voluptates? Corrupti eligendi deleniti veniam beatae cupiditate sit deserunt odio quibusdam aliquid id asperiores, animi excepturi nam adipisci!</h5>
+                                <!---->
+
+                                <div class="text-center mt-4 mb-4" data-aos="fade-up" data-aos-duration="4000">
+                                        <a href="../index.php"
+                                            class="btn mt-4 mb-4 fondo-color-indefinido-center tamaño-BotonRegreso-A-Casa rounded-circle"><img
+                                                src="../img/Icono.png" class="tamaño-imagen-boton mt-2" alt=""
+                                                srcset=""></a>
+                                    </div>
                         </div>
                 </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 
 </body>
 
